a99e4eb66645a9fab1d618e927697980b972ba29
hp <angelinatarasyuk6@gmail.com>
2025-01-18 11:42:55 +0200

    lesson44

--- lesson1/smartphone.py
+++ lesson1/smartphone.py
@@ -1,12 +1,14 @@
 class Computer:
     def __init__(self):
         self.memory =128
+
 class Display:
-            def __ init__(self):
-            self. resolution =n"4k"
+        def __init__(self):
+            self.resolution ="4k"
+
 class SmartPhone(Display, Computer):
                 def print_info(self):
-                    print (self resolution)
+                    print (self.resolution)
                     print(self.memory)
                     iphone = SmartPhone()
                     iphone.print_info()
